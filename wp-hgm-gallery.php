e3c2ed3f2a8abcf34a003e80db6521cd8d5531d6
David Carballo Izquierdo <161361087+CarballoDa@users.noreply.github.com>
2024-05-07 10:18:12 +0200

    Add files via upload

--- wp-hgm-gallery.php
+++ wp-hgm-gallery.php
@@ -13,4 +13,24 @@ Author URI: http://dicreaweb.com/
 License: GNUv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
+
+function my_plugin_activate() {
+
+    add_option( 'Activated_Plugin', 'hgm-gallery' );
+  
+    /* activation code here */
+  }
+  register_activation_hook( __FILE__, 'my_plugin_activate' );
+  
+  function load_plugin() {
+  
+      if ( is_admin() && get_option( 'Activated_Plugin' ) == 'hgm-gallery' ) {
+  
+          delete_option( 'hgm-gallery' );
+  
+          /* do stuff once right after activation */
+          // example: add_action( 'init', 'my_init_function' );
+      }
+  }
+  add_action( 'admin_init', 'load_plugin' );
 ?>
