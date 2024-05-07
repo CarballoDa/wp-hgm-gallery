6ec14ad4dbb38eb0f9e1e7e3cedbc0bd7e9ee7fc
David Carballo Izquierdo <161361087+CarballoDa@users.noreply.github.com>
2024-05-07 09:36:47 +0200

    Add files via upload

---
+++ wp-hgm-gallery.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package wp-hgm-gallery
+ * @version 0.0.1
+ */
+/*
+Plugin Name: WP hgm-gallery
+Plugin URI: http://wordpress.org/plugins/hgm-gallery/
+Description: This plugin allows to manage images with specific fields by categories
+Author: David Carballo Izquierdo <carballod@gmail.com>
+Version: 0.0.1
+Author URI: http://dicreaweb.com/
+License: GNUv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+?>
